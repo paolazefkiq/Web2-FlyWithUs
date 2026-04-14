f820d7b066c07188927e9a2a8f88c7fd3d45dc9b
Paola Zefkiq <paolazefkiq@gmail.com>
2026-04-14 22:13:06 +0200

    Shtimi i logos foto dhe emer

---
+++ includes/nav.php
@@ -0,0 +1,6 @@
+<?php $user = currentUser(); ?>
+<nav class="navbar">
+    <div class="logo">
+        <img src="<?= $GLOBALS['base_url'] ?>/assets/img/airplane-logo.png" alt="Airplane Logo" class="nav-logo">
+        <a href="<?= $GLOBALS['base_url'] ?>/index.php" class="logo-text">Fly With Us</a>
+    </div>
