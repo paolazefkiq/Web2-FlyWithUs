fc513ca6b86116fbc55bb9e10a0f164ada7e6d70
Paola Zefkiq <paolazefkiq@gmail.com>
2026-04-14 22:16:12 +0200

    Shtimi i hamburger menu-se

--- includes/nav.php
+++ includes/nav.php
@@ -4,3 +4,9 @@
         <img src="<?= $GLOBALS['base_url'] ?>/assets/img/airplane-logo.png" alt="Airplane Logo" class="nav-logo">
         <a href="<?= $GLOBALS['base_url'] ?>/index.php" class="logo-text">Fly With Us</a>
     </div>
+
+    <div class="hamburger" onclick="toggleMenu()">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
