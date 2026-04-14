06201c99fbf97cbda549f1841faf3369adf2ba46
Paola Zefkiq <paolazefkiq@gmail.com>
2026-04-14 22:18:48 +0200

    Shtimi i linkeve per navigacion

--- includes/nav.php
+++ includes/nav.php
@@ -10,3 +10,11 @@
         <span></span>
         <span></span>
     </div>
+
+    <div class="nav-links" id="navLinks">
+        <a href="<?= $GLOBALS['base_url'] ?>/index.php">Ballina</a>
+        <a href="<?= $GLOBALS['base_url'] ?>/index.php#booking">Rezervo</a>
+        <a href="<?= $GLOBALS['base_url'] ?>/index.php#destinations">Destinacionet</a>
+        <a href="<?= $GLOBALS['base_url'] ?>/pages/about.php">Rreth Nesh</a>
+        <a href="<?= $GLOBALS['base_url'] ?>/pages/faq.php">FAQ</a>
+        <a href="<?= $GLOBALS['base_url'] ?>/pages/contact.php">Kontakti</a>
