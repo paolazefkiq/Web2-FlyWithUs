895cf66a6c28b56476afa4701a5947a8dab99ca8
shqipezeneli <shqipe.zeneli3@student.uni-pr.edu>
2026-04-25 08:15:25 +0200

    update index

--- index.php
+++ index.php
@@ -10,4 +10,14 @@ $flashError = getFlash('flash_error');
 $popupSuccess = $_SESSION['popup_success'] ?? '';
 unset($_SESSION['popup_success']);
 $preferredCity = $_COOKIE['preferred_city'] ?? '';
+
+$old = [
+    'name' => '',
+    'email' => '',
+    'from' => '',
+    'to' => '',
+    'depart' => '',
+    'return' => '',
+    'passengers' => '1'
+];
 ?>
