32f20f89b087d452da52093b82e986c5c3ef8a54
Sumeja Ibrahimi <sumejaibrahimi08@gmail.com>
2026-04-26 00:14:20 +0200

    section (html)

--- pages/faq.php
+++ pages/faq.php
@@ -5,3 +5,13 @@ require_once __DIR__ . '/../includes/header.php';
 require_once __DIR__ . '/../includes/nav.php';
 ?>
 
+<section class="about-hero-bg">
+    <div class="hero-overlay">
+        <h1>Pyetje të Shpeshta</h1>
+        <p class="about-lead">
+            Këtu mund të gjeni përgjigje për pyetjet më të zakonshme rreth fluturimeve dhe rezervimeve.
+        </p>
+    </div>
+</section>
+
+
