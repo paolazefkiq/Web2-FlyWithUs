5c568b0a725540dafff3d021cf2729626787b1bb
Sumeja Ibrahimi <sumejaibrahimi08@gmail.com>
2026-04-26 00:10:08 +0200

    Fillimi per faq.php

---
+++ pages/faq.php
@@ -0,0 +1,7 @@
+<?php
+require once __DIR__ . '/../includes/config.php';
+$pageTitle = 'FAQ';
+require_once __DIR__ . '/../includes/header.php';
+require_once __DIR__ . '/../includes/nav.php';
+?>
+
