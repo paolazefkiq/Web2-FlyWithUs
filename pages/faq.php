f83a30e66b8474ed7bc0056e7c55ad85af9aa79b
Paola Zefkiq <paolazefkiq@gmail.com>
2026-04-26 17:49:28 +0200

    Rregullim sintakse

--- pages/faq.php
+++ pages/faq.php
@@ -1,5 +1,5 @@
 <?php
-require once __DIR__ . '/../includes/config.php';
+require_once __DIR__ . '/../includes/config.php';
 $pageTitle = 'FAQ';
 require_once __DIR__ . '/../includes/header.php';
 require_once __DIR__ . '/../includes/nav.php';
