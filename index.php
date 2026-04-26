2e5735305ac6402fed8a218e28be2faaf2a054f7
Paola Zefkiq <paolazefkiq@gmail.com>
2026-04-26 17:38:38 +0200

    Fshirja e pjeses se panevojshme

--- index.php
+++ index.php
@@ -5,8 +5,6 @@ require_once __DIR__ . '/includes/nav.php';
 
 // Marrja e te dhenave
 $sortedDestinations = sortDestinationsByPrice($destinations);
-$flashSuccess = getFlash('flash_success');
-$flashError = getFlash('flash_error');
 $popupSuccess = $_SESSION['popup_success'] ?? '';
 unset($_SESSION['popup_success']);
 $preferredCity = $_COOKIE['preferred_city'] ?? '';
@@ -121,14 +119,7 @@ $hasErrors = !empty(array_filter($errors));
 </header>
 
 <main>
-    <?php if ($flashSuccess): ?>
-        <div class="alert success"><?= e($flashSuccess) ?></div>
-    <?php endif; ?>
-
-    <?php if ($flashError): ?>
-        <div class="alert error"><?= e($flashError) ?></div>
-    <?php endif; ?>
-
+    
     <section class="info-strip">
         <div>
     <strong>Cookie preference:</strong>
