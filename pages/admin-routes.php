c5125b7dc1b1780f9f834090230f418d89b62872
shqipezeneli <shqipe.zeneli3@student.uni-pr.edu>
2026-05-25 20:16:41 +0200

    validimi i rrugeve

--- pages/admin-routes.php
+++ pages/admin-routes.php
@@ -24,3 +24,27 @@ $errors = [
     'destination_id' => '',
     'price' => '',
 ];
+
+$validateRouteForm = static function (array $data): array {
+    $errors = [
+        'origin_city_id' => '',
+        'destination_id' => '',
+        'price' => '',
+    ];
+
+    if ((int)$data['origin_city_id'] < 1) {
+        $errors['origin_city_id'] = 'Zgjidhni qytetin e nisjes.';
+    }
+
+    if ((int)$data['destination_id'] < 1) {
+        $errors['destination_id'] = 'Zgjidhni destinacionin.';
+    }
+
+    if ($data['price'] === '') {
+        $errors['price'] = 'Plotesoni cmimin.';
+    } elseif (!is_numeric($data['price']) || (float)$data['price'] <= 0) {
+        $errors['price'] = 'Cmimi duhet te jete numer pozitiv.';
+    }
+
+    return $errors;
+};
